c465730f564e70b4df74fa996cea4c6257c68b38
malvinnyahwai <malvinnyahwai@gmail.com>
2022-01-27 18:27:54 +0200

    Initial Commit

---
+++ src/main/java/com/malvin/becodingchallenge/BeCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.malvin.becodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCodingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeCodingChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/malvin/becodingchallenge/BeCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malvin.becodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeCodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
